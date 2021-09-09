1733584c759089bacdf899208e27b46a89ad4b9f
Atsighi Bright <brytebee@gmail.com>
2021-09-09 16:00:39 +0100

    Update index.js with sendData function

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
 import './styles.css';
-// import caller from './script.js'
 
-const scores = [];
+const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/03MSTYAna9kTLSUQIVGU/scores/';
 
-document.getElementById('submit').addEventListener('click', () => {
-  getData();
-});
-
-const getData = async() => {
-  const req = (await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/03MSTYAna9kTLSUQIVGU/scores/')).json();
+const getData = async () => {
+  const req = (await fetch(baseUrl)).json();
   const res = await req;
-  
+
   const scoreList = res.result;
   const pageList = document.getElementById('score-list');
-  scoreList.forEach(user => {
+  scoreList.forEach((user) => {
     const player = Object.values(user);
     // player.sort((a, b) => a - b)
     pageList.append(player[0], player[1]);
   });
 };
 
+const sendData = async () => {
+  const newScoreObj = {
+    "user": document.getElementById('name-input').value,
+    "score": document.getElementById('score-input').value
+  }
+  console.log(newScoreObj);
+  (await fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(newScoreObj),
+  })).json();
+  document.getElementById('name-input').value = '';
+  document.getElementById('score-input').value = '';
+};
+
+document.getElementById('submit').addEventListener('click', () => sendData());
+
+window.addEventListener('load', () => getData());
